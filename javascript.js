6c175b7939882b3a819eda271f895172f0d3f88e
Charles Dunn <charlesdunn@Charless-MBP.home>
2017-09-13 23:09:44 -0400

    more skeleton

--- javascript.js
+++ javascript.js
@@ -1,27 +1,14 @@
+  // var config = {
+  //   apiKey: "AIzaSyCrU6bBWJ-rkA80ytYVZl9mrFpk0N3zgYs",
+  //   authDomain: "myfirstproject-abadd.firebaseapp.com",
+  //   databaseURL: "https://myfirstproject-abadd.firebaseio.com",
+  //   projectId: "myfirstproject-abadd",
+  //   storageBucket: "myfirstproject-abadd.appspot.com",
+  //   messagingSenderId: "159759071992"
+  // };
+  // firebase.initializeApp(config);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+i = 4
 
 
 
@@ -32,7 +19,14 @@
 
 
 $("#submit-new").on("click", function(event){
-
-
 	event.preventDefault();
+
+	var newName = $("#new-name").val();
+	var newDest = $("#new-destination").val();
+	var newFirst = $("#new-first").val();
+	var newFreq = $("#new-frequency").val();
+
+	$("#tableBody").append('<tr><th scope="row">'+newName+'</th><td id="dest'+i+'">'+newDest+'</td><td id="freq'+i+'">#</td><td id="next3">00:00</td><td id="away'+i+'">'+i+'</td></tr>');
+	i++
+	
 })
